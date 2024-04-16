e094e6947a0f3029f83200cd2c49c6e32e19d228
Pikin <pikin@bk.ru>
2024-04-16 13:07:15 +0500

    client-server added

--- Client/src/main/java/client/App.java
+++ Client/src/main/java/client/App.java
@@ -1,7 +1,34 @@
 package client;
 
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Client");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите своё имя: ");
+        int port = 1300;
+        String name = scanner.nextLine();
+
+        try {
+            InetAddress address = InetAddress.getLocalHost();
+            System.out.println("Подключение к: " + address + ":" + port);
+            Socket socket = new Socket(address, port);
+            Client client = new Client(socket, name);
+            InetAddress inetAddress = socket.getInetAddress();
+            System.out.println("InetAddress: " + inetAddress);
+            String remoteIp = inetAddress.getHostAddress();
+            System.out.println("Remote IP: " + remoteIp);
+            System.out.println("LocalPort:" + socket.getLocalPort());
+            client.listenForMessage();
+            client.sendMessage();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
--- Server/src/main/java/server/App.java
+++ Server/src/main/java/server/App.java
@@ -1,7 +1,17 @@
 package server;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+
 public class App {
     public static void main(String[] args) {
-        System.out.printf("Server...");
+        try {
+            ServerSocket serverSocket = new ServerSocket(1300);
+            Server server = new Server(serverSocket);
+            server.runServer();
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
     }
 }
